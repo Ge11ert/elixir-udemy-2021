02a3d9ccfeb468df3df81b81ee6c21e55f9467d8
a.abramyants <a.abramyants@fun-box.ru>
2021-07-13 22:11:56 +0300

    first function

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,2 +1,5 @@
 defmodule Cards do
+  def create_desk do
+    ["Ace", "Two", "Three"]
+  end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,7 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "creates a card desk" do
+    assert Cards.create_desk() == ["Ace", "Two", "Three"]
   end
 end
