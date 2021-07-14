258fc5945e8907f4199c4ef9b6f820212eea574d
a.abramyants <a.abramyants@fun-box.ru>
2021-07-14 22:51:24 +0300

    new module functions

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -5,4 +5,9 @@ defmodule CardsTest do
   test "creates a card desk" do
     assert Cards.create_desk() == ["Ace", "Two", "Three"]
   end
+
+  test "checks if deck contains a card" do
+    assert Cards.contains?(Cards.create_desk(), "Two") == true
+    assert Cards.contains?(Cards.create_desk(), "King") == false
+  end
 end
