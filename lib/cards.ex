c23bd6fef9bf97f5e9b80476ec493a64f8f5ecfa
a.abramyants <a.abramyants@fun-box.ru>
2021-07-15 00:10:17 +0300

    save/load and pipe methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,5 @@
 defmodule Cards do
-  def create_desk do
+  def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
@@ -13,7 +13,29 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
-  def contains?(deck, hand) do
+  def contains?(deck, hand) do # function which return boolean should contain "?" at the end of the name
     Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size) # returns a tuple with two lists; [1, 2, 3] → {[1], [2,3]}
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "File does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
