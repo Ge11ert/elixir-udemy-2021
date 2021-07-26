1e811f9d4e3bc76ec51cf96172b5348ef54f8f70
a.abramyants <a.abramyants@fun-box.ru>
2021-07-26 19:30:54 +0300

    draw image

--- lib/identicon/identicon.ex
+++ lib/identicon/identicon.ex
@@ -7,6 +7,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_cells
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -36,6 +39,36 @@ defmodule Identicon do
     filtered = Enum.reject(grid, &removeCell?/1)
 
     %Identicon.Image{image | grid: filtered}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid, cell_size: cell_size, grid_size: grid_size} = image) do
+    pixel_map = Enum.map grid, fn({ _code, index}) ->
+      horizontal = rem(index, grid_size) * cell_size
+      vertical = div(index, grid_size) * cell_size
+
+      top_left = { horizontal, vertical }
+      bottom_right = { horizontal + cell_size, vertical + cell_size }
+
+      { top_left, bottom_right }
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map, cell_size: cell_size, grid_size: grid_size}) do
+    side_size = grid_size * cell_size
+    image = :egd.create(side_size, side_size)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 
   def mirror_rows(rows) do
--- lib/identicon/image.ex
+++ lib/identicon/image.ex
@@ -1,5 +1,8 @@
 defmodule Identicon.Image do
   defstruct hex: nil,
             color: nil,
-            grid: nil
+            grid: nil,
+            pixel_map: nil,
+            cell_size: 50,
+            grid_size: 5
 end
