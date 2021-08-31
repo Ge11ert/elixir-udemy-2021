5b2e1a6e2903c3f90dd97e1f01b4f21ba8e4b7b1
a.abramyants <a.abramyants@fun-box.ru>
2021-09-01 01:35:19 +0300

    Refactor topic controller.

--- discuss/lib/discuss/topics/topics.ex
+++ discuss/lib/discuss/topics/topics.ex
@@ -1,3 +1,20 @@
 defmodule Discuss.Topics do
 
+  alias Discuss.Topics.Topic
+  alias Discuss.Repo
+
+  def get_all do
+    Repo.all(Topic)
+  end
+
+  def get_new_changeset do
+    %Topic{}
+    |> Topic.changeset()
+  end
+
+  def create_topic(%{"topic" => topic}) do
+    %Topic{}
+    |> Topic.changeset(topic)
+    |> Repo.insert()
+  end
 end
--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,24 +1,19 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  alias Discuss.Topics.Topic
-  alias Discuss.Repo
+  alias Discuss.Topics
 
   def index(conn, _params) do
-    topics = Repo.all(Topic)
-    render(conn, "index.html", topics: topics)
+    render(conn, "index.html", topics: Topics.get_all())
   end
 
   def new(conn, _params) do
-    changeset = Topic.changeset(%Topic{}, %{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", changeset: Topics.get_new_changeset())
   end
 
-  def create(conn, %{"topic" => topic}) do
-    # TODO: нужно переместить это в context, в Discuss.Topics
-    changeset = Topic.changeset(%Topic{}, topic)
-    case Repo.insert(changeset) do
-      {:ok, post} -> render(conn, "new.html", post: post, changeset: changeset)
+  def create(conn, params) do
+    case Topics.create_topic(params) do
+      {:ok, post} -> render(conn, "new.html", post: post)
       {:error, changeset} -> render(conn, "new.html", changeset: changeset)
     end
   end
