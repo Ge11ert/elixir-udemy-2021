def053f23689f720dcff7c1287d5f1f639f989f4
a.abramyants <a.abramyants@fun-box.ru>
2021-09-01 00:47:27 +0300

    Handle topic creation.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,13 +2,19 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topics.Topic
+  alias Discuss.Repo
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(_conn, %{"topic" => topic}) do
-    IO.inspect topic
+  def create(conn, %{"topic" => topic}) do
+    # TODO: нужно переместить это в context, в Discuss.Topics
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} -> render(conn, "new.html", post: post, changeset: changeset)
+      {:error, changeset} -> render(conn, "new.html", changeset: changeset)
+    end
   end
 end
